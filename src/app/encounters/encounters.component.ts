0af1d4b6c02af8acf73484e0750159c018c1ecc8
justinl-y <j@solaris.org.uk>
2016-11-15 20:17:34 -0800

    Encounters.component.ts added

--- src/app/encounters/encounters.component.ts
+++ src/app/encounters/encounters.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Encounter } from '../models';
+import EncountersService from '../services/encounters.service';
 
-@Component({
+@Component( {
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
-  styleUrls: ['./encounters.component.css']
-})
+  styleUrls: ['./encounters.component.css'],
+  providers: [EncountersService],
+} )
 export class EncountersComponent implements OnInit {
+  marsEncounters: Encounter[];
 
-  constructor() { }
+  constructor( encountersService: EncountersService ) {
+    encountersService.getEncounters().subscribe( ( encounters ) => {
+      this.marsEncounters = encounters;
+    }, ( err ) => {
+      console.log( err );
+    });
+  }
 
   ngOnInit() {
   }
-
 }
